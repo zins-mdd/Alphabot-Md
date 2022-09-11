df3975b3d7948c47c86091b14caeb4cd7220f41b
zins-mdd <111218997+zins-mdd@users.noreply.github.com>
2022-09-11 11:12:23 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "MULTI BOTZ" //namabot kalian
-global.ownername= "ZINS STORE REAL" //nama kalian
+global.botname = "LIPPAY BOTZ" //namabot kalian
+global.ownername= "LINXX" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/channel/UC1SOSlVONbQV-o0NHNxu55w" //bebas asal jan hapus
+global.youtube = "https://instagram.com/_gussztian" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
 global.email = "zinsbotzmd@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['6282118415702'] //premium user
-global.owner = ["6282118415702","6282118415702"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6282118415702" // nomor wa kalian
-global.ownernomerr = "+6282118415702" //nmr wa kalian
+global.owner = ["6283126078265","6283126078265"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6283126078265" // nomor wa kalian
+global.ownernomerr = "+6283126078265" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© ZINS STORE REAL' //sticker wm ubah
-global.author = 'Di Buat Oleh ZINS STORE REAL' //sticker wm ganti nama kalian
+global.packname = '© LINXX' //sticker wm ubah
+global.author = 'Di Buat Oleh LINXX' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
